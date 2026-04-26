665c18d54c18d0bac2a02235c899398297162770
salih rıdvan yılmaz <185217082+sridvany@users.noreply.github.com>
2026-04-26 18:59:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ def clean_half_sentence(text):
 # ============================================================
 with st.sidebar:
     st.header("⚙️ Veri Ayarları")
-    ticker = st.text_input("Ticker Sembolü:", "^GSPC")
+    ticker = st.text_input("Ticker Sembolü:","PAXG-USD")
 
     period = st.selectbox(
         "Toplam Veri Süresi (Period):",
